83e06e2a40bedfd1203ee7e4d11694af7277fde3
HALLAYM LLC <158750555+abumafia@users.noreply.github.com>
2026-01-14 20:26:27 +0500

    Update jonimxbot.js

--- jonimxbot.js
+++ jonimxbot.js
@@ -7,7 +7,7 @@ const BOT_TOKEN = '8275520355:AAGNV6mPVOjkaNC4AtVXj8w-4h-xpvUZlrU';
 const MONGODB_URL = 'mongodb+srv://abumafia0:abumafia0@abumafia.h1trttg.mongodb.net/jonimxbot?appName=abumafia';
 
 // Adminlar ro'yxati
-const ADMIN_IDS = [6606638731, 901126203];
+const ADMIN_IDS = [6606638731, 901126203, 6457355191];
 
 // Render.com muhit o'zgaruvchilari
 const PORT = process.env.PORT || 10000;
